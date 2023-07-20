3073ab0cb1b93791a251b981264bf94be59a025c
baranero <jakub.baran2@gmail.com>
2023-07-20 20:12:07 +0200

    added animation on route change

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,26 @@
+import { delay, motion } from 'framer-motion'
+
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    width: '100%'
+  },
+  animate: {
+    x: '0%',
+    width: '0%'
+  },
+  exit: {
+    x: ['0%', '100%'],
+    width: ['0%', '100%']
+  }
+}
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#1a1333]' variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}>1</motion.div>
+      <div>2</div>
+      <div>3</div>
     </>
   );
 };
