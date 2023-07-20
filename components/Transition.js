4ab2e8e118829d18c94def4746ff939b983f8af5
baranero <jakub.baran2@gmail.com>
2023-07-20 23:05:09 +0200

    animations and design in homepage

--- components/Transition.js
+++ components/Transition.js
@@ -1,26 +1,53 @@
-import { delay, motion } from 'framer-motion'
+import { delay, motion } from "framer-motion";
 
 const transitionVariants = {
   initial: {
-    x: '100%',
-    width: '100%'
+    x: "100%",
+    width: "100%",
   },
   animate: {
-    x: '0%',
-    width: '0%'
+    x: "0%",
+    width: "0%",
   },
   exit: {
-    x: ['0%', '100%'],
-    width: ['0%', '100%']
-  }
-}
+    x: ["0%", "100%"],
+    width: ["0%", "100%"],
+  },
+};
 
 const Transition = () => {
   return (
     <>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#ffce0066]' variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{delay: 0.2, duration: 0.6, ease: 'easeInOut'}}>1</motion.div>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#ff9a0080]' variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{delay: 0.4, duration: 0.6, ease: 'easeInOut'}}>1</motion.div>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#ff5a0080]' variants={transitionVariants} initial='initial' animate='animate' exit='exit' transition={{delay: 0.6, duration: 0.6, ease: 'easeInOut'}}>1</motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#241a47]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2, duration: 0.6, ease: "easeInOut" }}
+      >
+        
+      </motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#2d2158]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4, duration: 0.6, ease: "easeInOut" }}
+      >
+        1
+      </motion.div>
+      <motion.div
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#c089404e]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6, duration: 0.6, ease: "easeInOut" }}
+      >
+        1
+      </motion.div>
     </>
   );
 };
