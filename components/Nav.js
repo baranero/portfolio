4ebb956fb2392d66df14800ab577fcb3ee148cdc
baranero <jakub.baran2@gmail.com>
2023-07-20 18:48:45 +0200

    navigation component design

--- components/Nav.js
+++ components/Nav.js
@@ -5,10 +5,10 @@ import {
   HiViewColumns,
   HiChatBubbleBottomCenterText,
   HiEnvelope,
-} from "react-icons/hi";
+} from "react-icons/hi2";
 
 //  links
-const links = [
+const navData = [
   { name: "home", path: "/", icon: <HiHome /> },
   { name: "about", path: "/about", icon: <HiUser /> },
   { name: "services", path: "/services", icon: <HiRectangleGroup /> },
@@ -25,8 +25,26 @@ const links = [
   },
 ];
 
+import Link from "next/link";
+
+import { useRouter } from "next/router";
+
 const Nav = () => {
-  return <nav>nav</nav>;
+  const router = useRouter();
+  const pathname = router.pathname;
+  return (
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen">
+      <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
+        {navData.map((link, index) => {
+          return (
+            <Link className={`${link.path === pathname && 'text-accent'}`} key={index} href={link.path}>
+              <div>{link.icon}</div>
+            </Link>
+          );
+        })}
+      </div>
+    </nav>
+  );
 };
 
 export default Nav;
