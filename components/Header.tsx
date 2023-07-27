9e3b3307ecaaaf04c2ec90d6de7a306e927c07c2
baranero <jakub.baran2@gmail.com>
2023-07-27 11:58:50 +0200

    bg changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,27 @@
-import React from 'react'
-import Image from 'next/image';
-import Link from 'next/link';
-import Socials from '../components/Socials'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Socials from "../components/Socials";
 
 const Header = () => {
-  return <header className="backdrop-blur-md md:backdrop-blur-none lg:backdrop-blur-none xl:backdrop-blur-none absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
-    <div className='container mx-auto'>
-      <div className='flex flex-col lg:flex-row justify-between items-center gap-y-2 py-2'>
-        <Link href={'/'}>
-          <Image src={'/download.svg'} width={220} height={48} alt='Logo' priority={true}/>
-        </Link>
-        <Socials/>
+  return (
+    <header className="backdrop-blur-md md:backdrop-blur-none lg:backdrop-blur-none xl:backdrop-blur-none absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-y-2 py-2">
+          <Link href={"/"}>
+            <Image
+              src={"/download.svg"}
+              width={220}
+              height={48}
+              alt="Logo"
+              priority={true}
+            />
+          </Link>
+          <Socials />
+        </div>
       </div>
-    </div>
-  </header>;
+    </header>
+  );
 };
 
 export default Header;
